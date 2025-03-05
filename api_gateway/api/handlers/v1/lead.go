8658dc88718648ee85e4289da8eb28340ba22dbb
abdulaziz omonov <abdulaziomonov4@gmail.com>
2025-03-05 20:52:54 +0500

    payment service restart

--- api_gateway/api/handlers/v1/lead.go
+++ api_gateway/api/handlers/v1/lead.go
@@ -2,14 +2,12 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/golanguzb71/livesphere-api-gateway/pkg/etc"
 )
 
 func (h *handlerV1) CreateLead(c *gin.Context) {
-	title := c.Query("title")
-	ctx, cancelFunc := etc.NewTimoutContext(c)
-	defer cancelFunc()
-
+	//title := c.Query("title")
+	//ctx, cancelFunc := etc.NewTimoutContext(c)
+	//defer cancelFunc()
 }
 
 func (h *handlerV1) GetLeadCommon(c *gin.Context) {
--- api_gateway/services/main.go
+++ api_gateway/services/main.go
@@ -1,15 +1,46 @@
 package services
 
-import "github.com/golanguzb71/livesphere-api-gateway/config"
+import (
+	"github.com/golanguzb71/livesphere-api-gateway/config"
+)
 
 type ServiceManager interface {
 	AuthService() AuthServiceI
+	CoreService() CoreServiceI
+	PaymentService() PaymentServiceI
 }
 
 type grpcClients struct {
-	authService AuthServiceI
+	authService    AuthServiceI
+	coreService    CoreServiceI
+	paymentService PaymentServiceI
 }
 
 func NewGrpcClients(conf *config.Config) (ServiceManager, error) {
-	return &grpcClients{}, nil
+	authService, err := NewAuthService(conf)
+	if err != nil {
+		return nil, err
+	}
+	coreService, err := NewCoreService(conf)
+	if err != nil {
+		return nil, err
+	}
+	paymentService, err := NewPaymentService(conf)
+	if err != nil {
+		return nil, err
+	}
+	return &grpcClients{
+		authService:    authService,
+		coreService:    coreService,
+		paymentService: paymentService,
+	}, nil
+}
+func (g *grpcClients) AuthService() AuthServiceI {
+	return g.authService
+}
+func (g *grpcClients) CoreService() CoreServiceI {
+	return g.coreService
+}
+func (g *grpcClients) PaymentService() PaymentServiceI {
+	return g.paymentService
 }
